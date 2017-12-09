f080a69f38297de54152ad47858deb9564b43d12
Steve Mostovoy <stevemostovoysm@gmail.com>
2017-12-08 18:28:13 -0800

    Day 3 part 2.

--- src/day_3.rs
+++ src/day_3.rs
@@ -1,17 +1,28 @@
+use std::collections::HashMap;
+
 use utils;
 
 pub fn run() {
     for line in utils::read_input(3) {
         let number = line.parse::<u32>().unwrap();
-        let point = get_spiral_coords(number);
+        let point = compute_spiral_point_up_to(number);
         let distance = point.x.abs() + point.y.abs();
         println!("{}", distance);
+
+        let first_value_greater_than_number = compute_first_value_greater_than_number(number);
+        println!("{}", first_value_greater_than_number);
     }
 }
 
 struct Point {
     x: i32,
     y: i32,
+}
+
+impl Point {
+    fn to_string(&self) -> String {
+        return [self.x.to_string(), self.y.to_string()].join(",");
+    }
 }
 
 struct Bounds {
@@ -46,7 +57,7 @@ impl Bounds {
     }
 }
 
-fn get_spiral_coords(number: u32) -> Point {
+fn compute_spiral_point_up_to(number: u32) -> Point {
     let mut point = Point { x: 0, y: 0 };
     let mut bounds = Bounds {
         left: 0,
@@ -54,22 +65,78 @@ fn get_spiral_coords(number: u32) -> Point {
         top: 0,
         bottom: 0,
     };
+
     for _ in 2..number + 1 {
-        if bounds.bottom_blocked(&point) && bounds.right_blocked(&point) {
-            bounds.expand();
-        }
-        if bounds.bottom_blocked(&point) {
-            point.x += 1;
-        } else if bounds.left_blocked(&point) {
-            point.y += 1;
-        } else if bounds.top_blocked(&point) {
-            point.x -= 1;
-        } else if bounds.right_blocked(&point) {
-            point.y -= 1;
-        } else {
-            point.x += 1;
-        }
+        iterate_spiral(&mut point, &mut bounds);
     }
 
     return point;
+}
+
+fn compute_first_value_greater_than_number(number: u32) -> u32 {
+    let mut point = Point { x: 0, y: 0 };
+    let mut bounds = Bounds {
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+    };
+
+    let mut spiral = HashMap::new();
+
+    for _ in 2..number + 1 {
+        let mut value = sum_adjacents(&point, &spiral);
+        if value == 0 {
+            value = 1;
+        }
+        if value > number {
+            return value;
+        }
+        spiral.insert(point.to_string(), value);
+
+        iterate_spiral(&mut point, &mut bounds);
+    }
+
+    return 0;
+}
+
+fn iterate_spiral(point: &mut Point, bounds: &mut Bounds) {
+    if bounds.bottom_blocked(&point) && bounds.right_blocked(&point) {
+        bounds.expand();
+    }
+    if bounds.bottom_blocked(&point) {
+        point.x += 1;
+    } else if bounds.left_blocked(&point) {
+        point.y += 1;
+    } else if bounds.top_blocked(&point) {
+        point.x -= 1;
+    } else if bounds.right_blocked(&point) {
+        point.y -= 1;
+    } else {
+        point.x += 1;
+    }
+}
+
+fn sum_adjacents(point: &Point, spiral: &HashMap<String, u32>) -> u32 {
+    let adjacent_points: [Point; 9] = [
+        Point { x: point.x - 1, y: point.y - 1},
+        Point { x: point.x - 1, y: point.y },
+        Point { x: point.x - 1, y: point.y + 1},
+        Point { x: point.x, y: point.y - 1},
+        Point { x: point.x, y: point.y },
+        Point { x: point.x, y: point.y + 1},
+        Point { x: point.x + 1, y: point.y - 1},
+        Point { x: point.x + 1, y: point.y },
+        Point { x: point.x + 1, y: point.y + 1},
+    ];
+
+    let mut sum : u32 = 0;
+    for point in adjacent_points.iter() {
+        match spiral.get(&point.to_string()) {
+            Some(value) => sum += value,
+            None => {},
+        }
+    }
+
+    return sum;
 }
