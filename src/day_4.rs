d1834de5744e2bf23ce55f44ebc72c9435383a92
Steve Mostovoy <stevemostovoysm@gmail.com>
2017-12-28 00:29:54 -0500

    Make day 4 output both part 1 and 2.

--- src/day_4.rs
+++ src/day_4.rs
@@ -6,22 +6,36 @@ use utils;
 
 pub fn run() {
     let mut total_valid_passphrases = 0;
+    let mut total_valid_passphrases_sorted = 0;
     for line in utils::read_input(4) {
         let words: Vec<String> = line.split(' ').map(String::from).collect();
-        let mut unique_words = HashSet::new();
-        let mut valid = true;
-        for word in words {
-            let sorted_word = word.chars().sorted();
-
-            if unique_words.contains(&sorted_word) {
-                valid = false;
-                break;
-            }
-            unique_words.insert(sorted_word);
-        }
-        if valid {
+        if is_valid(&words) {
             total_valid_passphrases += 1;
         }
+        if is_valid(&words.iter()
+                          .map(|word| word.chars()
+                                          .sorted()
+                                          .iter()
+                                          .collect())
+                          .collect()) {
+            total_valid_passphrases_sorted += 1;
+        }
     }
+
     println!("{}", total_valid_passphrases);
+    println!("{}", total_valid_passphrases_sorted);
+}
+
+fn is_valid(words: &Vec<String>) -> bool {
+    let mut unique_words = HashSet::new();
+    let mut valid = true;
+    for word in words {
+        if unique_words.contains(&word) {
+            valid = false;
+            break;
+        }
+        unique_words.insert(word);
+    }
+
+    valid
 }
